05ce4ce54f8c38672e7ba213e94ef35b08579ba9
Edinson Requena <edinsonrequena@protonmail.com>
2024-01-06 19:54:50 -0400

    chore: intial config

---
+++ tiktok_clone_back/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tiktok_clone_back project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tiktok_clone_back.settings')
+
+application = get_asgi_application()
